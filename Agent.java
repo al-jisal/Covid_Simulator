5df1d6bef8758434316b2dc43042c57fa5dab525
Desmond Frimpong <117947706+al-jisal@users.noreply.github.com>
2023-08-08 07:20:29 -0400

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -2,7 +2,7 @@
      * Date: February 24, 2023
      * File: Agent.java
      * 
-     * this class implement an entity called Agent in a spatial coordinates (x,y)
+     * this class implement an abstract entity called Agent in a spatial coordinates (x,y)
      */
     
 
